54e6b989632b014701df4d5b9905c39b40777bb8
Daniel Kowalski <kowal256@gmail.com>
2014-10-23 15:43:34 +0200

    added xrange

--- tasks/zaj2/zadanie1.py
+++ tasks/zaj2/zadanie1.py
@@ -6,3 +6,11 @@ def xrange(start_stop, stop=None, step=None):
     Funkcja która działa jak funkcja range (wbudowana i z poprzednich zajęć)
     która działa dla liczb całkowitych.
     """
+    start = start_stop
+    if not stop:
+        start = 0
+        stop = start_stop
+
+    while start < stop:
+        yield start
+        start += (step if step else 1)
